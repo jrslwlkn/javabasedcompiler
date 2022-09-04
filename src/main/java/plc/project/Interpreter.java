e0b823c45a4b652fda9dfc19b963c631bcc3bf2c
Yareeque <jrslwlkn@gmail.com>
2022-09-04 15:44:20 -0500

    Interpreter: Add files pertaining to P3

---
+++ src/main/java/plc/project/Interpreter.java
@@ -0,0 +1,141 @@
+package plc.project;
+
+public class Interpreter implements Ast.Visitor<Environment.PlcObject> {
+
+    private Scope scope = new Scope(null);
+
+    public Interpreter(Scope parent) {
+        scope = new Scope(parent);
+        scope.defineFunction("print", 1, args -> {
+            System.out.println(args.get(0).getValue());
+            return Environment.NIL;
+        });
+    }
+
+    public Scope getScope() {
+        return scope;
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Source ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Field ast) {
+        if (ast.getValue().isPresent()) {
+            scope.defineVariable(ast.getName(), visit(ast.getValue().get()));
+        } else {
+            scope.defineVariable(ast.getName(), Environment.NIL);
+        }
+
+        return Environment.NIL;
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Method ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Stmt.Expression ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Stmt.Declaration ast) {
+        if (ast.getValue().isPresent()) {
+            scope.defineVariable(ast.getName(), visit(ast.getValue().get()));
+        } else {
+            scope.defineVariable(ast.getName(), Environment.NIL);
+        }
+
+        return Environment.NIL;
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Stmt.Assignment ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Stmt.If ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Stmt.For ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Stmt.While ast) {
+        while (requireType(Boolean.class, visit(ast.getCondition()))) {
+            try {
+                scope = new Scope(scope);
+                for (Ast.Stmt stmt : ast.getStatements()) {
+                    visit(stmt);
+                }
+            } finally {
+                scope = scope.getParent();
+            }
+        }
+
+        return Environment.NIL;
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Stmt.Return ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Expr.Literal ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Expr.Group ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Expr.Binary ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Expr.Access ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    @Override
+    public Environment.PlcObject visit(Ast.Expr.Function ast) {
+        throw new UnsupportedOperationException(); //TODO
+    }
+
+    /**
+     * Helper function to ensure an object is of the appropriate type.
+     */
+    private static <T> T requireType(Class<T> type, Environment.PlcObject object) {
+        if (type.isInstance(object.getValue())) {
+            return type.cast(object.getValue());
+        } else {
+            throw new RuntimeException("Expected type " + type.getName() + ", received " + object.getValue().getClass().getName() + ".");
+        }
+    }
+
+    /**
+     * Exception class for returning values.
+     */
+    private static class Return extends RuntimeException {
+
+        private final Environment.PlcObject value;
+
+        private Return(Environment.PlcObject value) {
+            this.value = value;
+        }
+
+    }
+
+}
