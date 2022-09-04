ba56020b491eceb5508f51609eb6ea708bcbf134
Yareeque <jrslwlkn@gmail.com>
2022-09-04 15:44:19 -0500

    Add missing test file

---
+++ src/test/java/plc/project/LexerTests.java
@@ -0,0 +1,214 @@
+package plc.project;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class LexerTests {
+
+    @ParameterizedTest
+    @MethodSource
+    void testIdentifier(String test, String input, boolean success) {
+        test(input, Token.Type.IDENTIFIER, success);
+    }
+
+    private static Stream<Arguments> testIdentifier() {
+        return Stream.of(
+                Arguments.of("Alphabetic", "getName", true),
+                Arguments.of("Alphanumeric", "thelegend27", true),
+                Arguments.of("Leading Hyphen", "-five", false),
+                Arguments.of("Leading Digit", "1fish2fish3fishbluefish", false),
+                Arguments.of("Contains Hyphen", "identifier-", true),
+                Arguments.of("Underscores", "___", true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testInteger(String test, String input, boolean success) {
+        test(input, Token.Type.INTEGER, success);
+    }
+
+    private static Stream<Arguments> testInteger() {
+        return Stream.of(
+                Arguments.of("Single Digit", "1", true),
+                Arguments.of("Signed Integer", "+1", true),
+                Arguments.of("Decimal", "123.456", false),
+                Arguments.of("Signed Decimal", "-1.0", false),
+                Arguments.of("Trailing Decimal", "1.", false),
+                Arguments.of("Leading Decimal", ".5", false),
+                Arguments.of("Plus Zero", "+0", true),
+                Arguments.of("Minus Zero", "-0", true),
+                Arguments.of("Double Zero", "00", true),
+                Arguments.of("Leading Zeros", "+0001", true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testDecimal(String test, String input, boolean success) {
+        test(input, Token.Type.DECIMAL, success);
+    }
+
+    private static Stream<Arguments> testDecimal() {
+        return Stream.of(
+                Arguments.of("Integer", "1", false),
+                Arguments.of("Multiple Digits", "123.456", true),
+                Arguments.of("Negative Decimal", "-1.0", true),
+                Arguments.of("Positive Decimal", "+1.0", true),
+                Arguments.of("Trailing Decimal", "1.", false),
+                Arguments.of("Leading Decimal", ".5", false),
+                Arguments.of("Plus Zero", "+0.00", true),
+                Arguments.of("Minus Zero", "-0.00", true),
+                Arguments.of("Leading Zeros", "0001.0", true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testCharacter(String test, String input, boolean success) {
+        test(input, Token.Type.CHARACTER, success);
+    }
+
+    private static Stream<Arguments> testCharacter() {
+        return Stream.of(
+                Arguments.of("Alphabetic", "'c'", true),
+                Arguments.of("Non-alphabetic", "'@'", true),
+                Arguments.of("Single Quote Escaped", "'\\''", true),
+                Arguments.of("Single Quote Unescaped", "'''", false),
+                Arguments.of("Newline Escape", "'\\n'", true),
+                Arguments.of("Single Escape", "'\\'", false),
+                Arguments.of("Escaped Backslash", "'\\\\'", true),
+                Arguments.of("Empty", "''", false),
+                Arguments.of("Multiple", "'abc'", false),
+                Arguments.of("Number", "'1'", true),
+                Arguments.of("Space", "' '", true),
+                Arguments.of("Backspace", "'\\b'", true),
+                Arguments.of("Just Opening Single quote", "'", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testString(String test, String input, boolean success) {
+        test(input, Token.Type.STRING, success);
+    }
+
+    private static Stream<Arguments> testString() {
+        return Stream.of(
+                Arguments.of("Empty", "\"\"", true),
+                Arguments.of("Alphabetic", "\"abc\"", true),
+                Arguments.of("Newline Escape", "\"Hello,\\nWorld\"", true),
+                Arguments.of("Unterminated", "\"unterminated", false),
+                Arguments.of("Invalid Escape", "\"invalid\\escape\"", false),
+                Arguments.of("Unescaped Double quote", "\"\"\"", false),
+                Arguments.of("Escaped Double quote", "\"\\\"\"", true),
+                Arguments.of("Symbols", "\"!@#$%^&*()\"", true),
+                Arguments.of("Just Opening Double quote", "\"", false)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testOperator(String test, String input, boolean success) {
+        //this test requires our lex() method, since that's where whitespace is handled.
+        test(input, Arrays.asList(new Token(Token.Type.OPERATOR, input, 0)), success);
+    }
+
+    private static Stream<Arguments> testOperator() {
+        return Stream.of(
+                Arguments.of("Character", "(", true),
+                Arguments.of("Comparison", "<=", true),
+                Arguments.of("Space", " ", false),
+                Arguments.of("Tab", "\t", false),
+                Arguments.of("Equals Sign", "=", true),
+                Arguments.of("Divide", "/", true)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    void testExamples(String test, String input, List<Token> expected) {
+        test(input, expected, true);
+    }
+
+    private static Stream<Arguments> testExamples() {
+        return Stream.of(
+                Arguments.of("Example 1", "LET x = 5;", Arrays.asList(
+                        new Token(Token.Type.IDENTIFIER, "LET", 0),
+                        new Token(Token.Type.IDENTIFIER, "x", 4),
+                        new Token(Token.Type.OPERATOR, "=", 6),
+                        new Token(Token.Type.INTEGER, "5", 8),
+                        new Token(Token.Type.OPERATOR, ";", 9)
+                )),
+                Arguments.of("Example 2", "print(\"Hello, World!\");", Arrays.asList(
+                        new Token(Token.Type.IDENTIFIER, "print", 0),
+                        new Token(Token.Type.OPERATOR, "(", 5),
+                        new Token(Token.Type.STRING, "\"Hello, World!\"", 6),
+                        new Token(Token.Type.OPERATOR, ")", 21),
+                        new Token(Token.Type.OPERATOR, ";", 22)
+                )),
+                Arguments.of("Example 3", "const abc = +( a > b\n)\n\r", Arrays.asList(
+                        new Token(Token.Type.IDENTIFIER, "const", 0),
+                        new Token(Token.Type.IDENTIFIER, "abc", 6),
+                        new Token(Token.Type.OPERATOR, "=", 10),
+                        new Token(Token.Type.OPERATOR, "+", 12),
+                        new Token(Token.Type.OPERATOR, "(", 13),
+                        new Token(Token.Type.IDENTIFIER, "a", 15),
+                        new Token(Token.Type.OPERATOR, ">", 17),
+                        new Token(Token.Type.IDENTIFIER, "b", 19),
+                        new Token(Token.Type.OPERATOR, ")", 21)
+                ))
+        );
+    }
+
+    @Test
+    void testException() {
+        ParseException exception = Assertions.assertThrows(ParseException.class,
+                () -> new Lexer("\"unterminated").lex());
+        Assertions.assertEquals(13, exception.getIndex());
+
+        exception = Assertions.assertThrows(ParseException.class,
+                () -> new Lexer("\"invalid\\escape\"").lex());
+        Assertions.assertEquals(9, exception.getIndex());
+    }
+
+    /**
+     * Tests that lexing the input through {@link Lexer#lexToken()} produces a
+     * single token with the expected type and literal matching the input.
+     */
+    private static void test(String input, Token.Type expected, boolean success) {
+        try {
+            if (success) {
+                Assertions.assertEquals(new Token(expected, input, 0), new Lexer(input).lexToken());
+            } else {
+                Assertions.assertNotEquals(new Token(expected, input, 0), new Lexer(input).lexToken());
+            }
+        } catch (ParseException e) {
+            Assertions.assertFalse(success, e.getMessage());
+        }
+    }
+
+    /**
+     * Tests that lexing the input through {@link Lexer#lex()} matches the
+     * expected token list.
+     */
+    private static void test(String input, List<Token> expected, boolean success) {
+        try {
+            if (success) {
+                Assertions.assertEquals(expected, new Lexer(input).lex());
+            } else {
+                Assertions.assertNotEquals(expected, new Lexer(input).lex());
+            }
+        } catch (ParseException e) {
+            Assertions.assertFalse(success, e.getMessage());
+        }
+    }
+
+}
